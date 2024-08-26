ab7b3defac7f999d02d3de22989a215db601dcff
Muhammadumairtanveer <umair799284@gmail.com>
2024-08-26 11:42:01 +0500

    color_picker

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ColorPicker from './ColorPicker'
+
+const App = () => {
+  return (
+    <>
+    
+    <ColorPicker></ColorPicker>
+
+
+
+    </>
+  )
+}
+
+export default App
